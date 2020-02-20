8c440a0fbe24a94218281db3951ea62598986b2a
T3chy <elam.day.friedland@gmail.com>
2020-02-20 15:14:36 -0500

    ok train ride done now will work later

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
-#import yfinance as yf
+from yahoo_fin import stock_info as si 
 import csv
+def add(ticker):
+	while ticker == "":
+		ticker = input("ticker?")
+	currentprice = si.get_live_price(ticker)
+	print("the current price for " + ticker + " is " +str(currentprice))
+	while conf == "":
+		conf = input("does this look right? y/n")
+	if conf == "n":
+		price = input("please input the price you bought at")
+	else:
+		price = currentprice
+	countshares = input("how many shares did you purchase?")
+	print(""
+Ticker | Price | Shares
+----------------------"" 
++ ticker + " | " + price + " | " countshares)
+#	with open(r'idscores.csv', 'a') as f:
+#		writer = csv.writer(f)
+#		writer.writerow(keyandscores)
 choice = ""
 while 'TRUE' == 'TRUE':
 	choice = input("""
 Welcome to Elam's Portfolio! Please select a function :)
 --------------------------------------------------------
-Add a trade	View Trades	View Portfolio
-[add]		[viewtrades]	[viewportfolio]
+Add a trade	View Trades	View Portfolio    View Information on a Stock
+[add]		[viewtrades]	[viewportfolio]		    [info]
 Your Selection:""")
 	if choice == "add":
 		break
-	if choice == "viewtrades"
+	if choice == "viewtrades":
 		break
-	if choice == "viewportfolio"
+	if choice == "viewportfolio":
 		break
-print("your choice is " + str(choice))
+if choice == "add"
+
