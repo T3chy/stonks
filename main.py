2b3dfae7d83c7cdba69c4677c47d9e2070290318
T3chy <elam.day.friedland@gmail.com>
2020-02-20 14:55:22 -0500

    first commit bois

---
+++ main.py
@@ -0,0 +1,17 @@
+#import yfinance as yf
+import csv
+choice = ""
+while 'TRUE' == 'TRUE':
+	choice = input("""
+Welcome to Elam's Portfolio! Please select a function :)
+--------------------------------------------------------
+Add a trade	View Trades	View Portfolio
+[add]		[viewtrades]	[viewportfolio]
+Your Selection:""")
+	if choice == "add":
+		break
+	if choice == "viewtrades"
+		break
+	if choice == "viewportfolio"
+		break
+print("your choice is " + str(choice))
